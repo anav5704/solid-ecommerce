2106b1605a239a8e5f4ac9fd88967f0ab3d41c15
anav5704 <anavkki@gmail.com>
2024-01-19 19:51:49 +1200

    build: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import Card from './components/card';
 
 const App: Component = () => {
     return (
-        <div>
-            <header>
-                <img style={{height: "45px", width: "45px"}} src="/src/logo.svg" alt="" />
-                <h1>Solid Store</h1>
+        <div class='bg-zinc-900 min-h-screen text-white'>
+            <header class='flex items-center gap-x-3 p-5'>
+                <img class='h-10 w-10' src="/src/logo.svg" alt="" />
+                <h1 class='font-bold text-2xl bg-gradient-to-tr from-sky-700 to-blue-400 bg-clip-text text-transparent'>Solid Store</h1>
             </header>
             <Card />
         </div>
