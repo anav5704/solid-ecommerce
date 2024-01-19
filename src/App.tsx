07a49b59daa8da46cfda3d78021293f15580c43a
anav5704 <anavkki@gmail.com>
2024-01-19 20:20:30 +1200

    feat: use createSignl to show content

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import type { Component } from 'solid-js'
+import { createSignal, type Component } from 'solid-js'
 import Card from './components/card'
 import logo from './logo.svg'
 
 const App: Component = () => {
+    const [show, setShow] = createSignal(true)
+
     return (
         <div class='bg-zinc-950 min-h-screen text-white'>
             <header class='flex items-center gap-x-3 px-20 py-5'>
                 <img class='h-10 w-10' src="/src/logo.svg" alt="" />
                 <h1 class='font-bold text-2xl bg-gradient-to-tr from-sky-700 to-blue-400 bg-clip-text text-transparent'>Solid Store</h1>
             </header>
-            <main class='grid grid-cols-4 m-20'>
+            { show() && ( <main class='grid grid-cols-4 m-20'>
                 <Card
                     title="Solid Bra"
                     description="A bra made by the Solid JS team"
                 />
-            </main>
+            </main> )}
         </div>
     )
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,11 +2,11 @@ import { ComponentProps } from "solid-js"
 
 export default function Card(props: { title: string, description: string }) {
     return (
-        <div class="col-span-1 p-3 bg-zinc-900 rounded-xl">
+        <div class="col-span-1 p-3 bg-zinc-900/75 border border-zinc-800 rounded-xl">
             <h2 class="text-xl font-semibold">{props.title}</h2>
             <p>{props.description}</p>
             <div class="flex justify-end">
-            <button class="mt-3 bg-gradient-to-tr from-sky-700 to-blue-400 px-6 py-2 rounded-lg">Buy</button>
+            <button class="mt-3 bg-gradient-to-tr from-sky-700 to-blue-400 px-6 py-2 rounded-xl">Buy</button>
             </div>
         </div>
     )
