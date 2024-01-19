6fc4c7926caf3c742671844734ee212d2b1959f8
anav5704 <anavkki@gmail.com>
2024-01-19 20:13:23 +1200

    feat: pass content to card using props

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,13 @@
-export default function Card() {
+import { ComponentProps } from "solid-js"
+
+export default function Card(props: { title: string, description: string }) {
     return (
-        <div>
-            <h2>Name</h2>
-            <p>Description</p>
+        <div class="col-span-1 p-3 bg-zinc-900 rounded-xl">
+            <h2 class="text-xl font-semibold">{props.title}</h2>
+            <p>{props.description}</p>
+            <div class="flex justify-end">
+            <button class="mt-3 bg-gradient-to-tr from-sky-700 to-blue-400 px-6 py-2 rounded-lg">Buy</button>
+            </div>
         </div>
     )
 }
